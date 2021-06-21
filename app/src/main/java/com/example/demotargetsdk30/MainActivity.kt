a053821efc833ee3d31b5655fc4f60b7d3dafb60
Arti <arti@classplus.co>
2021-06-21 16:57:47 +0530

    Changelog: -Used coroutine instead of thread

--- app/src/main/java/com/example/demotargetsdk30/MainActivity.kt
+++ app/src/main/java/com/example/demotargetsdk30/MainActivity.kt
@@ -2,22 +2,35 @@ package com.example.demotargetsdk30
 
 import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.os.Handler
+import android.os.Environment
 import android.provider.MediaStore
+import android.provider.Settings
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.demotargetsdk30.databinding.ActivityMainBinding
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.lifecycleScope
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
-import java.util.zip.Inflater
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+import com.example.demotargetsdk30.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,27 +63,37 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
         intent.addCategory(Intent.CATEGORY_OPENABLE)
         intent.type = "image/jpeg"
-        startActivityForResult(intent, PICK_DOC)
+        startActivityForResult(intent, PICK_IMAGES)
     }
 
     private fun requestPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             permissionsToRequire?.add(Manifest.permission.READ_EXTERNAL_STORAGE)
         }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             permissionsToRequire?.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         }
         if (permissionsToRequire?.isNotEmpty()!!) {
             permissionsToRequire?.toTypedArray()?.let {
-                ActivityCompat.requestPermissions(this,
-                    it, 0)
+                ActivityCompat.requestPermissions(
+                    this,
+                    it, 0
+                )
             }
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        when(resultCode){
+        when (requestCode) {
             PICK_DOC -> {
                 if (resultCode == Activity.RESULT_OK && data != null) {
                     val uri = data.data
@@ -93,7 +116,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun copyUriToExternalFilesDir(uri: Uri, fileName: String) {
-        Handler().postDelayed({
+        lifecycleScope.launch(Dispatchers.IO){
             val inputStream = contentResolver.openInputStream(uri)
             val tempDir = getExternalFilesDir("temp")
             if (inputStream != null && tempDir != null) {
@@ -110,22 +133,27 @@ class MainActivity : AppCompatActivity() {
                 }
                 bos.close()
                 fos.close()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "Copy file into $tempDir succeeded.", Toast.LENGTH_LONG).show()
+                }
             }
-            Toast.makeText(this, "Copy file into $tempDir succeeded.", Toast.LENGTH_LONG).show()
-        },0)
+        }
     }
+
 
     private fun getFileNameByUri(uri: Uri): String {
         var fileName = System.currentTimeMillis().toString()
         val cursor = contentResolver.query(uri, null, null, null, null)
         if (cursor != null && cursor.count > 0) {
             cursor.moveToFirst()
-            fileName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME))
+            fileName =
+                cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME))
             cursor.close()
         }
         return fileName
     }
-    companion object{
+
+    companion object {
         const val PICK_DOC = 1
         const val PICK_IMAGES = 2
     }
